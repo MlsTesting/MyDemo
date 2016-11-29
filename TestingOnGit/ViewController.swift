73ca0bcdffea83cb12f68b42f3e80928f9b9b2f2
Bunty <Bunty@MacMini.local>
2016-11-29 17:03:31 +0530

    this is changes in file

--- TestingOnGit/ViewController.swift
+++ TestingOnGit/ViewController.swift
@@ -12,12 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        //hello this is changes if file
+        print("viewDidLoad")
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+        
+        print("didReceiveMemoryWarning")
+
+    
     }
 
 
